c0ad089bc6594efc8e5efa8eade93b66fd001a75
kibo13 <kimboris1310@gmail.com>
2022-04-10 21:55:31 +0600

    refactor: updated icons code

--- resources/views/components/icons/toggle.blade.php
+++ resources/views/components/icons/toggle.blade.php
@@ -1,4 +1,5 @@
 <svg xmlns="http://www.w3.org/2000/svg"
+     class="navbar-btn__icon"
      fill="none"
      viewBox="0 0 24 24"
      stroke="currentColor">
@@ -6,5 +7,5 @@
     <path stroke-linecap="round"
           stroke-linejoin="round"
           stroke-width="2"
-          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+          d="M4 6h16M4 12h8m-8 6h16" />
 </svg>
