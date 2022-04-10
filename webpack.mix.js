a82c2b466112580c9722ae25b2e52d73a860c675
kibo13 <kimboris1310@gmail.com>
2022-04-10 16:26:52 +0600

    perf: updated settings of webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,23 +5,10 @@ mix .js("resources/js/auth/index.js", "public/js/auth.js")
     .sass("resources/sass/auth/index.sass", "public/css/auth.css")
     .version();
 
-// site
-// mix.js("resources/js/site/index.js", "public/js/site.js");
-// mix.sass("resources/sass/site.sass", "public/css/site.css").options({
-//     postCss: [
-//         require("autoprefixer"),
-//         // require(...)
-//     ],
-// });
-
 // admin
-// mix.js("resources/js/admin/index.js", "public/js/admin.js");
-// mix.sass("resources/sass/admin/index.sass", "public/css/admin.css").options({
-//     postCss: [
-//         require("autoprefixer"),
-//         // require(...)
-//     ],
-// });
+mix .js("resources/js/admin/index.js", "public/js/admin.js")
+    .sass("resources/sass/admin/index.sass", "public/css/admin.css")
+    .version();
 
 if (!mix.inProduction()) {
     mix.browserSync("dance.test");
