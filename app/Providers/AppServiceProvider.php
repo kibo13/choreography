517cdd57d9691e15de371677d0774eca6c4c4c61
kibo13 <kimboris1310@gmail.com>
2022-04-10 21:51:30 +0600

    feat: added directives to track active of links in navbar and sidebar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // sidebar tracking
+        Blade::directive('sbactive', function ($route) {
+            return "<?php echo Route::currentRouteNamed($route) ? 'class=\"active\"' : '' ?>";
+        });
+
+        // navbar tracking
+        Blade::directive('nbactive', function ($route) {
+            return "<?php echo Route::currentRouteNamed($route) ? 'class=\"active\"' : '' ?>";
+        });
     }
 }
