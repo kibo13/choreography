fcdedb4a6a58c0705326327b87cd26092d62d6ce
Boris Kim <kimboris1310@gmail.com>
2022-04-14 17:01:49 +0600

    feat: added vue.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ const mix = require('laravel-mix');
 
 // auth
 mix .js("resources/js/auth/index.js", "public/js/auth.js")
+    .vue()
     .sass("resources/sass/auth/index.sass", "public/css/auth.css")
     .version();
 
