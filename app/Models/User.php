ce5ad66b52e62bcbd7c0f4b240bb8e295adcb605
Boris Kim <kimboris1310@gmail.com>
2022-04-15 17:31:16 +0600

    feat: set up relationship between tables (roles, permissions, users)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -26,4 +25,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function hasPermission(...$permissions)
+    {
+        foreach ($permissions as $permission) {
+            if ($this->permissions->contains('slug', $permission)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
