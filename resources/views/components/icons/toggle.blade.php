e885eb0993c4024c42fbb6a4bce9cfeebc6a4e3b
Boris Kim <kimboris1310@gmail.com>
2022-04-15 17:01:07 +0600

    refactor: cleaned up svg-icon code

--- resources/views/components/icons/toggle.blade.php
+++ resources/views/components/icons/toggle.blade.php
@@ -1,11 +1,9 @@
 <svg xmlns="http://www.w3.org/2000/svg"
-     class="navbar-btn__icon"
-     fill="none"
+     class="h-6 w-6" fill="none"
      viewBox="0 0 24 24"
-     stroke="currentColor">
-
+     stroke="currentColor"
+     stroke-width="1">
     <path stroke-linecap="round"
           stroke-linejoin="round"
-          stroke-width="2"
           d="M4 6h16M4 12h8m-8 6h16" />
 </svg>
