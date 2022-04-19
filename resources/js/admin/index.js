b3827ed31fee306562ec6553cd6bd18e2b271b44
Boris Kim <kimboris1310@gmail.com>
2022-04-19 18:20:02 +0600

    feat: added datatables-net

--- resources/js/admin/index.js
+++ resources/js/admin/index.js
@@ -1,5 +1,10 @@
 // vendors
-require('../bootstrap')
+require('bootstrap');
+window.$ = require("jquery");
+window.dt = require('datatables.net');
+
+// custom
+require('./custom/datatables-net')
 
 // partials
 require('./partials/navbar')
