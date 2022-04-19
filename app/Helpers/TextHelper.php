4175dc49655437e24df45ce9925cc1799dfdc08b
kibo13 <kimboris1310@gmail.com>
2022-04-20 01:14:24 +0600

    feat: added new methods to helpers

--- app/Helpers/TextHelper.php
+++ app/Helpers/TextHelper.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+
 function form_title($param)
 {
     echo $param ?? null
@@ -15,4 +17,25 @@ function mandatory()
 function tip($message)
 {
     echo '<span class="bk-tip">' . $message .'</i>';
+}
+
+function short_fio($id)
+{
+    $user = User::where('id', $id)->first();
+
+    $first_name     = ucfirst($user->first_name);
+    $last_name      = substr($user->last_name, 0, 2) . '.';
+
+    echo $first_name . ' ' . $last_name;
+}
+
+function full_fio($id)
+{
+    $user = User::where('id', $id)->first();
+
+    $first_name     = ucfirst($user->first_name);
+    $last_name      = substr($user->last_name, 0, 2) . '.';
+    $middle_name    = isset($user->middle_name) ? substr($user->middle_name, 0, 2) . '.' : null;
+
+    echo $first_name . ' ' . $last_name . $middle_name;
 }
