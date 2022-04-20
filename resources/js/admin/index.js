78c70f49cc629a00170b478c6326f65354015018
kibo13 <kimboris1310@gmail.com>
2022-04-21 02:13:25 +0600

    feat: added hide alert after some time

--- resources/js/admin/index.js
+++ resources/js/admin/index.js
@@ -7,6 +7,7 @@ window.dt = require('datatables.net');
 require('./components/sidebar')
 require('./components/datatable')
 require('./components/modal-delete')
+require('./components/alert')
 
 // partials
 require('./partials/sidebar')
