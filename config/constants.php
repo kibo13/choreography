82e982f2d3310d9e11c83ef09455b19e8666e2e6
Boris Kim <kimboris1310@gmail.com>
2022-04-22 13:17:44 +0600

    feat: added constants

---
+++ config/constants.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    // default password
+    'password'  => '123456',
+    // documents
+    'docs'      => [
+        0               => 'Уд.личности',
+        1               => 'Паспорт',
+    ],
+];
