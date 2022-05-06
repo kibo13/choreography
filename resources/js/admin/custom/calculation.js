06962fe327f304cc338ef126be798817256026ab
kibo13 <kimboris1310@gmail.com>
2022-05-06 19:03:36 +0600

    refactor: cleaned up code to calculation.js

--- resources/js/admin/custom/calculation.js
+++ resources/js/admin/custom/calculation.js
@@ -1,12 +1,11 @@
 export const calc = {
     age(date) {
-        let today       = new Date();
-        let birthday    = new Date(date);
-        let age         = today.getFullYear() - birthday.getFullYear();
-        let m           = today.getMonth() - birthday.getMonth();
-        let condition   = m === 0 && today.getDate() < birthday.getDate()
+        let today     = new Date();
+        let birthday  = new Date(date);
+        let age       = today.getFullYear() - birthday.getFullYear();
+        let month     = today.getMonth() - birthday.getMonth();
+        let condition = month < 0 || month === 0 && today.getDate() < birthday.getDate()
 
-        if (m < 0 || condition) age--;
-        return age;
+        return condition ? age-- : age;
     }
 }
