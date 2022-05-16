2c8fd0cfd27e0583f1e6800844be9f64118574f1
Boris Kim <kimboris1310@gmail.com>
2022-05-16 12:19:18 +0600

    feat: added chart.js lib

---
+++ app/Charts/MasterChart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Charts;
+
+use ConsoleTVs\Charts\Classes\Chartjs\Chart;
+
+class MasterChart extends Chart
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
--- resources/views/admin/pages/achievements/graph.blade.php
+++ resources/views/admin/pages/achievements/graph.blade.php
@@ -1,1 +1,4 @@
-{{ __('_dialog.underway') }}
+<div style="width: 100%; height: 400px;">
+    {!! $chart->container() !!}
+    {!! $chart->script() !!}
+</div>
