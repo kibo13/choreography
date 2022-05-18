22a8f273cb8539cb442e69c7eab74ea5707443fe
kibo13 <kimboris1310@gmail.com>
2022-05-18 22:47:38 +0600

    refactor: cleaned up code to webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,16 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 
 // auth
-mix .js("resources/js/auth/index.js", "public/js/auth.js")
+mix .js('resources/js/auth/index.js', 'public/js/auth.js')
     .vue()
-    .sass("resources/sass/auth/index.sass", "public/css/auth.css")
-    .version();
+    .sass('resources/sass/auth/index.sass', 'public/css/auth.css')
+    .version()
 
 // admin
-mix .js("resources/js/admin/index.js", "public/js/admin.js")
-    .sass("resources/sass/admin/index.sass", "public/css/admin.css")
-    .version();
+mix .js('resources/js/admin/index.js', 'public/js/admin.js')
+    .sass('resources/sass/admin/index.sass', 'public/css/admin.css')
+    .version()
 
 if (!mix.inProduction()) {
-    mix.browserSync("dance.test");
+    mix.browserSync('dance.test')
 }
